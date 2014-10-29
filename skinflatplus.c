521a6dc17ed9fc515e5f1bc8bfaf95ce04e83e8c
Martin Schirrmacher <vdr.skinflatplus@schirrmacher.eu>
2014-10-29 20:12:42 +0100

    release version 0.4.3

--- skinflatplus.c
+++ skinflatplus.c
@@ -16,7 +16,7 @@
 #include "setup.h"
 #include "imageloader.h"
 
-static const char *VERSION        = "0.4.2";
+static const char *VERSION        = "0.4.3";
 static const char *DESCRIPTION    = "skin flatplus";
 
 class cPluginFlat : public cPlugin {
