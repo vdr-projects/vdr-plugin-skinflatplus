df3e9f1854af5efe1b253dac54a73ff6043aa2c1
Martin Schirrmacher <vdr.skinflatplus@schirrmacher.eu>
2015-02-08 14:23:20 +0100

    [add] recording menu, progressbar icon to show how much seen from the recording

--- displaymenu.c
+++ displaymenu.c
@@ -1932,6 +1932,42 @@ bool cFlatDisplayMenu::SetItemRecording(const cRecording *Recording, int Index, 
                     menuIconsPixmap->DrawImage( cPoint(Left, Top), *imgRecNew );
                 }
             }
+            #if APIVERSNUM >= 20108
+            else {
+                int FrameTotal = Recording->NumFrames();
+                int FrameResume = Recording->GetResume();
+                double FrameSeen = (double)FrameResume / (double)FrameTotal;
+                cString SeenIcon;
+
+                if( FrameSeen < 0.1 )
+                    SeenIcon = "recording_seen_0";
+                else if( FrameSeen < 0.2 )
+                    SeenIcon = "recording_seen_1";
+                else if( FrameSeen < 0.3 )
+                    SeenIcon = "recording_seen_2";
+                else if( FrameSeen < 0.4 )
+                    SeenIcon = "recording_seen_3";
+                else if( FrameSeen < 0.5 )
+                    SeenIcon = "recording_seen_4";
+                else if( FrameSeen < 0.6 )
+                    SeenIcon = "recording_seen_5";
+                else if( FrameSeen < 0.7 )
+                    SeenIcon = "recording_seen_6";
+                else if( FrameSeen < 0.8 )
+                    SeenIcon = "recording_seen_7";
+                else if( FrameSeen < 0.9 )
+                    SeenIcon = "recording_seen_8";
+                else if( FrameSeen < 0.98 )
+                    SeenIcon = "recording_seen_9";
+                else
+                    SeenIcon = ""; // Do not show seenicon if recording is full seen
+
+                cImage *imgSeen = imgLoader.LoadIcon(SeenIcon, fontHeight, fontHeight);
+                if( imgSeen != NULL )
+                    menuIconsPixmap->DrawImage( cPoint(Left, Top), *imgSeen );
+            }
+            #endif
+
             Left += imgRecNew->Width() + marginItem;
             if (Recording->IsEdited()) {
                 if( imgRecCut ) {
@@ -2018,6 +2054,42 @@ bool cFlatDisplayMenu::SetItemRecording(const cRecording *Recording, int Index, 
                     menuIconsPixmap->DrawImage( cPoint(Left, Top), *imgRecNew );
                 }
             }
+            #if APIVERSNUM >= 20108
+            else {
+                int FrameTotal = Recording->NumFrames();
+                int FrameResume = Recording->GetResume();
+                double FrameSeen = (double)FrameResume / (double)FrameTotal;
+                cString SeenIcon;
+
+                if( FrameSeen < 0.1 )
+                    SeenIcon = "recording_seen_0";
+                else if( FrameSeen < 0.2 )
+                    SeenIcon = "recording_seen_1";
+                else if( FrameSeen < 0.3 )
+                    SeenIcon = "recording_seen_2";
+                else if( FrameSeen < 0.4 )
+                    SeenIcon = "recording_seen_3";
+                else if( FrameSeen < 0.5 )
+                    SeenIcon = "recording_seen_4";
+                else if( FrameSeen < 0.6 )
+                    SeenIcon = "recording_seen_5";
+                else if( FrameSeen < 0.7 )
+                    SeenIcon = "recording_seen_6";
+                else if( FrameSeen < 0.8 )
+                    SeenIcon = "recording_seen_7";
+                else if( FrameSeen < 0.9 )
+                    SeenIcon = "recording_seen_8";
+                else if( FrameSeen < 0.98 )
+                    SeenIcon = "recording_seen_9";
+                else
+                    SeenIcon = ""; // Do not show seenicon if recording is full seen
+
+                cImage *imgSeen = imgLoader.LoadIcon(SeenIcon, fontHeight, fontHeight);
+                if( imgSeen != NULL )
+                    menuIconsPixmap->DrawImage( cPoint(Left, Top), *imgSeen );
+            }
+            #endif
+
             Left += imgRecNew->Width() + marginItem;
             if (Recording->IsEdited()) {
                 if( imgRecCut ) {
